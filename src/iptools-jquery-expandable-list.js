4a75911e393f88340a04aa269c5c3989ca06f9b3
David Lehnen <dl@interactive-pioneers.de>
2015-07-01 15:48:32 +0200

    Remove blank lines

--- src/iptools-jquery-expandable-list.js
+++ src/iptools-jquery-expandable-list.js
@@ -64,15 +64,11 @@
   };
 
   $.fn[pluginName] = function(options) {
-
     return this.each(function() {
-
       if (!$.data(this, 'plugin_' + pluginName)) {
         $.data(this, 'plugin_' + pluginName, new IPTExpandableList(this, options));
       }
-
     });
-
   };
 
 })(jQuery);
